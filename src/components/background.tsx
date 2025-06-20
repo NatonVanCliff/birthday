2145b8564ecdf6b21d6ae1336efc00922c150519
Maksim Torkanovskii <naton_van_cliff@mail.ru>
2025-06-20 16:06:59 +0300

    Fixed bug with image url

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -5,7 +5,7 @@ export const Background = styled.div`
     height: 100%;
     width: 100%;
 
-    background-image: url(/images/sky.svg), url(/images/mountains.svg);
+    background-image: url(./images/sky.svg), url(./images/mountains.svg);
     background-position: top, bottom;
     background-repeat: no-repeat, no-repeat;
     background-size: auto 50vh, 100vw 25vh;
